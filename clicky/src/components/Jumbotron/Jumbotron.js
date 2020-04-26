858c298b070d0590b008d09fc82efe76914641ea
Leah <leahdodson9@gmail.com>
2020-04-26 15:07:07 -0500

    completed jumotron component

--- clicky/src/components/Jumbotron/Jumbotron.js
+++ clicky/src/components/Jumbotron/Jumbotron.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import "./Jumbotron.css";
 
-const Jumbotron = props => (
+const Jumbotron = () => (
 <div class="jumbotron">
     <h1 class="display-4 title">Clicky Game</h1>
     <hr class="my-4" />
     <p>Click on an image to earn points, but don't click on any image more than once!</p>
 </div>
-
-    )
+    );
 
 export default Jumbotron;
