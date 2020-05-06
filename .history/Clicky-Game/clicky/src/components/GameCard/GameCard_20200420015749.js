1a252408e907618640f713f46572d5acc19b67f0
Leah <leahdodson9@gmail.com>
2020-05-06 00:15:27 -0500

    updated gitignore

--- .history/Clicky-Game/clicky/src/components/GameCard/GameCard_20200420015749.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import "./GameCard.css";
-
-const GameCard = props => (
-    <div class="game-card col-xs-6 col-sm-6 col-md-4 col-lg-2 col-xl-2"> 
-    <img alt={props.name} src={props.image}>
-    </div>
-
-)
